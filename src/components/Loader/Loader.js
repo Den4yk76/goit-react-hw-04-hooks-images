abcdc008e0471198ea999610d6b0d62d36c1d367
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-10-22 18:47:48 +0300

    Loader to hooks

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,14 +1,10 @@
-import { Component } from 'react';
-
-export default class Loader extends Component {
-  render() {
-    return (
-      <div className="lds-ellipsis">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    );
-  }
+export default function Loader() {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
