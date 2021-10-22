6987eb0b5912936e2a45a6813fe2aedce56b87fd
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-10-22 18:50:02 +0300

    Button to hooks

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,18 +1,15 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Button extends Component {
-  handleClick = () => {
-    return this.props.onClick(1);
+export default function Button({ onClick }) {
+  const handleClick = () => {
+    return onClick(1);
   };
 
-  render() {
-    return (
-      <button onClick={this.handleClick} className="Button" type="button">
-        Load More!
-      </button>
-    );
-  }
+  return (
+    <button onClick={handleClick} className="Button" type="button">
+      Load More!
+    </button>
+  );
 }
 
 Button.propTypes = {
